034133717320f0eaafc0e17d4f5a7610f272b515
francois-gonon <francois.gonon@epfedu.fr>
2025-05-22 12:34:58 +0200

    TEST PUSH

--- simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "François") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "TEST") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 
